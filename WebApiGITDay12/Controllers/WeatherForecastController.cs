881f40d451e45678c17daace0739e45188408b27
Alladin07 <uzalladin@gmail.com>
2024-10-09 12:30:22 +0530

    Added summary action and modified weather forecast controller

--- WebApiGITDay12/Controllers/WeatherForecastController.cs
+++ WebApiGITDay12/Controllers/WeatherForecastController.cs
@@ -18,7 +18,8 @@ namespace WebApiGITDay12.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
+        [Route("forecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -28,6 +29,13 @@ namespace WebApiGITDay12.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet]
+        [Route("summary")]
+        public IEnumerable<string> GetSummary()
+        {
+            return Summaries;
         }
     }
 }
