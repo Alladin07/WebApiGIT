bb1475bc10ce2d1a7a2069f902b0883fbc706001
Alladin07 <uzalladin@gmail.com>
2024-10-09 12:39:58 +0530

    Added ValuesController

---
+++ WebApiGITDay12/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiGITDay12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("today")]
+        public IActionResult Get()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
